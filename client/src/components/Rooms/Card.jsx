658eff4473f71de69acaafdf2ec110c2cecc3e4b
Arefin Aareef <arefin.aareef@gmail.com>
2023-09-16 03:14:53 +0600

    swipper added

--- client/src/components/Rooms/Card.jsx
+++ client/src/components/Rooms/Card.jsx
@@ -1,51 +1,94 @@
-import { Link } from 'react-router-dom'
-import HeartButton from '../Button/HeartButton'
+import { Link } from "react-router-dom";
+import HeartButton from "../Button/HeartButton";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
 
-const Card = ({room}) => {
+import { Navigation } from "swiper/modules";
+
+const Card = ({ room }) => {
   return (
-    <Link to={`/room/${1}`} className='col-span-1 cursor-pointer group'>
-      <div className='flex flex-col gap-2 w-full'>
+    <Link to={`/room/${1}`} className="col-span-1 cursor-pointer group">
+      <div className="flex flex-col gap-2 w-full">
         <div
-          className='
+          className="
             aspect-square 
             w-full 
+            
             relative 
             overflow-hidden 
             rounded-xl
-          '
+          "
         >
-          <img
-            className='
+          <Swiper
+            navigation={true}
+            modules={[Navigation]}
+            className="mySwiper object-cover 
+            h-full 
+            w-full"
+          >
+            <SwiperSlide>
+              <img
+                className="
               object-cover 
               h-full 
               w-full 
               group-hover:scale-110 
               transition
-            '
-            src={room.image}
-            alt='Room'
-          />
+            "
+                src={room.image}
+                alt="Room"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="
+              object-cover 
+              h-full 
+              w-full 
+              group-hover:scale-110 
+              transition
+            "
+                src={room.image}
+                alt="Room"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="
+              object-cover 
+              h-full 
+              w-full 
+              group-hover:scale-110 
+              transition
+            "
+                src={room.image}
+                alt="Room"
+              />
+            </SwiperSlide>
+          </Swiper>
+
           <div
-            className='
+            className="
             absolute
             top-3
             right-3
-          '
+          "
           >
             <HeartButton />
           </div>
         </div>
-        <div className='font-semibold text-lg'>{room.location}</div>
-        <div className='font-light text-neutral-500'>
-          5 nights . {room.dateRange}
+        <div className="font-semibold text-lg">{room.location}</div>
+        <div className="font-light text-neutral-500">
+          5 nights . {room.availableFrom} - {room.availableTo}
         </div>
-        <div className='flex flex-row items-center gap-1'>
-          <div className='font-semibold'>$ {room.price}</div>
-          <div className='font-light'>night</div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">$ {room.price}</div>
+          <div className="font-light">night</div>
         </div>
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
